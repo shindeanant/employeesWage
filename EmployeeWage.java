d3e2a1fc3daaf9896a098e9cd5e287a2ce6b6ad9
shindeanant <shindeanant1997@gmail.com>
2021-10-08 12:11:04 +0530

    ADD:Calculated emp wage for each company

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,107 +1,56 @@
 package com.employeeswage;
 
-import java.util.Random;
-
 public class EmployeeWage {
-	int wagePerHours = 20;
-	int dailyHours = 8;
-	int dailyWage;
-	int partTimeWage;
-	int partTimeHours = 4;
-	int employeeHour;
-	int daysPerMonth = 20;
-	int monthlyWage;
-	int totalHours = 100;
-	int totalDays = 20;
+	public static final int isPartTime = 1;
+	public static final int isFullTime = 2;
+	public static final int empRatePerHours = 20;
+	public static final int numberOfWorkingDays = 20;
+	public static final int maxHoursIntMonth = 100;
 
-	public void employeeAttdence() {
-		int empCheck = (int) (Math.random() * 10) % 2;
-		System.out.println(empCheck);
-		if (empCheck == 1) {
-			System.out.println("Employee is Present");
-		} else {
-			System.out.println("Employee is Absent");
-		}
-	}
-
-	public void dailyWage() {
-		dailyWage = wagePerHours * dailyHours;
-		System.out.println("Employee Daily wage = " + dailyWage);
-	}
-
-	public void employeePartTimeWage() {
-
-		partTimeWage = wagePerHours * partTimeHours;
-		System.out.println("Employee Part Time Wage :" + partTimeWage);
-	}
-
-	public void bySwitchCase() {
-		Random ran = new Random();
-		int empCheck = ran.nextInt(3);
-		switch (empCheck) {
-		case 0:
-			employeeHour = 8;
-			System.out.print("Present");
-			break;
-		case 1:
-			employeeHour = 4;
-			System.out.print("Part Time");
-			break;
-		case 2:
-			employeeHour = 0;
-			System.out.println("Absent");
-			break;
-		}
-		System.out.println();
-	}
-
-	public void monthlyWage() {
-
-		monthlyWage = dailyWage * daysPerMonth;
-		System.out.println("Monthly Wage Of Employee is =" + monthlyWage);
-	}
-
-	public void calculateWagesTillGivenCond() {
-
+	public static void main(String[] args) {
+		int empHr;
 		int empWage = 0;
 		int totalEmpWage = 0;
 		int totalWorkingDays = 0;
-		int totalEmpHours = 0;
+		int totalEmpHrs = 0;
 
-		while (totalEmpHours <= totalHours && totalWorkingDays < totalDays) {
+		System.out.print("Welcome to Employee Wage Computation Program on Master Branch");
+		while (totalEmpHrs <= maxHoursIntMonth && totalWorkingDays < numberOfWorkingDays) {
+
 			totalWorkingDays++;
 			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+			;
+			/** int rand_int2 = rand.nextInt(1000); */
+
+			/** Print random integers */
 			System.out.println("Random Integers: " + empCheck);
+			// System.out.println("Random Integers: "+rand_int2);
+
 			switch (empCheck) {
 			case 1:
-				employeeHour = 8;
-				System.out.print(" Present");
+				empHr = 8;
+				System.out.print("Full time");
 				break;
 			case 2:
-				employeeHour = 4;
-				System.out.print("Part time Present ");
+				empHr = 4;
+				System.out.print("Part time");
 				break;
 			case 0:
-				employeeHour = 0;
+				empHr = 0;
 				System.out.println("Absent");
 				break;
+
+			default:
+				empHr = 0;
+				break;
 			}
-			empWage = employeeHour * wagePerHours;
+			empWage = empHr * empRatePerHours;
 			totalEmpWage = totalEmpWage + empWage;
-			System.out.println("Day " + totalWorkingDays + " empHours :" + employeeHour);
-			System.out.println(" Wage :" + totalEmpWage);
-			System.out.println(" Total Wage :" + totalEmpWage);
+			System.out.println("Days" + totalWorkingDays + "emphrs" + empHr);
+			System.out.println("Wage" + totalEmpWage);
 		}
+
+		System.out.println("Total wage" + totalEmpWage);
 	}
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Employee Wage Computation Program");
-		EmployeeWage object = new EmployeeWage();
-		object.employeeAttdence();
-		object.dailyWage();
-		object.employeePartTimeWage();
-		object.bySwitchCase();
-		object.monthlyWage();
-		object.calculateWagesTillGivenCond();
-	}
 }
