63488f9d9df274d3f6bbf8fd4dcf52f51a20cb3c
shindeanant <shindeanant1997@gmail.com>
2021-10-08 11:55:39 +0530

    ADD: Calculate employee wage till hundred hours

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -11,6 +11,8 @@ public class EmployeeWage {
 	int employeeHour;
 	int daysPerMonth = 20;
 	int monthlyWage;
+	int totalHours = 100;
+	int totalDays = 20;
 
 	public void employeeAttdence() {
 		int empCheck = (int) (Math.random() * 10) % 2;
@@ -59,6 +61,39 @@ public class EmployeeWage {
 		System.out.println("Monthly Wage Of Employee is =" + monthlyWage);
 	}
 
+	public void calculateWagesTillGivenCond() {
+
+		int empWage = 0;
+		int totalEmpWage = 0;
+		int totalWorkingDays = 0;
+		int totalEmpHours = 0;
+
+		while (totalEmpHours <= totalHours && totalWorkingDays < totalDays) {
+			totalWorkingDays++;
+			int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+			System.out.println("Random Integers: " + empCheck);
+			switch (empCheck) {
+			case 1:
+				employeeHour = 8;
+				System.out.print(" Present");
+				break;
+			case 2:
+				employeeHour = 4;
+				System.out.print("Part time Present ");
+				break;
+			case 0:
+				employeeHour = 0;
+				System.out.println("Absent");
+				break;
+			}
+			empWage = employeeHour * wagePerHours;
+			totalEmpWage = totalEmpWage + empWage;
+			System.out.println("Day " + totalWorkingDays + " empHours :" + employeeHour);
+			System.out.println(" Wage :" + totalEmpWage);
+			System.out.println(" Total Wage :" + totalEmpWage);
+		}
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		EmployeeWage object = new EmployeeWage();
@@ -67,5 +102,6 @@ public class EmployeeWage {
 		object.employeePartTimeWage();
 		object.bySwitchCase();
 		object.monthlyWage();
+		object.calculateWagesTillGivenCond();
 	}
 }
