d84d8186869b237c0013d24ce3099a5fac501231
shindeanant <shindeanant1997@gmail.com>
2021-10-08 11:11:12 +0530

    ADd:Calculating employee part time wage

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -4,6 +4,8 @@ public class EmployeeWage {
 	int wagePerHours = 20;
 	int dailyHours = 8;
 	int dailyWage;
+	int partTimeWage;
+	int partTimeHours = 4;
 
 	public void employeeAttdence() {
 		int empCheck = (int) (Math.random() * 10) % 2;
@@ -20,10 +22,17 @@ public class EmployeeWage {
 		System.out.println("Employee Daily wage = " + dailyWage);
 	}
 
+	public void employeePartTimeWage() {
+
+		partTimeWage = wagePerHours * partTimeHours;
+		System.out.println("Employee Part Time Wage :" + partTimeWage);
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		EmployeeWage object = new EmployeeWage();
 		object.employeeAttdence();
 		object.dailyWage();
+		object.employeePartTimeWage();
 	}
 }
